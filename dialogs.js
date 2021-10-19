51ab5ac9398f0319d967c1f5633682675de1a999
Paola Facchin <83193050+paola-facchin@users.noreply.github.com>
2021-10-19 11:56:21 -0300

    Add files via upload

---
+++ dialogs.js
@@ -0,0 +1,39 @@
+function abreDialog(){
+ document.getElementById("d1").style.top = "0";
+}
+function fechaDialog(){
+    document.getElementById("d1").style.top= "-200%"
+}
+
+function abreDialog2(){
+    document.getElementById("d2").style.top = "0";
+   }
+   function fechaDialog2(){
+       document.getElementById("d2").style.top= "-200%"
+   }
+
+   function abreDialog3(){
+    document.getElementById("d3").style.top = "0";
+   }
+   function fechaDialog3(){
+       document.getElementById("d3").style.top= "-200%"
+   }
+
+   function abredialogvideo(){
+    document.getElementById("v1").style.top = "0";
+   }
+   function fechadialogvideo(){
+       document.getElementById("v1").style.top= "-200%"
+   }
+   function abredialogvideo2(){
+    document.getElementById("v2").style.top = "0";
+   }
+   function fechadialogvideo2(){
+       document.getElementById("v2").style.top= "-500%"
+   }
+   function abredialogvideo3(){
+    document.getElementById("v3").style.top = "0";
+   }
+   function fechadialogvideo3(){
+       document.getElementById("v3").style.top= "-200%"
+   }
